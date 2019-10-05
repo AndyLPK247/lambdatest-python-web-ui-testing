5f0154a29a946b5111bac22ff77b99f66e65c3e0
AndyLPK247 <AndyLPK247@gmail.com>
2019-10-05 16:20:47 -0400

    added test_search

---
+++ tests/test_search.py
@@ -0,0 +1,30 @@
+"""
+These tests cover DuckDuckGo searches.
+"""
+
+import pytest
+
+from pages.result import DuckDuckGoResultPage
+from pages.search import DuckDuckGoSearchPage
+
+
+@pytest.mark.parametrize('phrase', ['panda', 'python', 'polar bear'])
+def test_basic_duckduckgo_search(browser, phrase):
+  search_page = DuckDuckGoSearchPage(browser)
+  result_page = DuckDuckGoResultPage(browser)
+  
+  # Given the DuckDuckGo home page is displayed
+  search_page.load()
+
+  # When the user searches for the phrase
+  search_page.search(phrase)
+
+  # Then the search result query is the phrase
+  assert phrase == result_page.search_input_value()
+
+  # And the search result title contains the phrase
+  assert phrase.lower() in result_page.title()
+  
+  # And the search result links pertain to the phrase
+  for title in result_page.result_link_titles():
+    assert phrase.lower() in title.lower()
